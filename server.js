c5ae2e1a6c0baea127dfd462bc1ba04f4859f3a7
Glitch (glitch-hello-node) <none>
2021-10-09 13:17:31 +0000

    😎🚦 Checkpoint ./server.js:24664144/30

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/nhansu", async (req, res) => {
     const readData = await googleSheetsInstance.spreadsheets.values.get({
         auth, //auth object
         spreadsheetId, // spreadsheet id
-        range: "Sheet1!A2:17", //range of cells to read from.
+        range: "Sheet1!A2:I2", //range of cells to read from.
     })
 
     //send the data reae with the response
